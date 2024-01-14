f0f1fedef5e5ebe6de7232471445a467fdcf0336
happyMan <sebasgrisales16@gmail.com>
2024-01-14 16:01:10 -0500

    feat: public forlder created.

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import {env} from './config';
 
 const app  = express();
-const PORT = 4000;
+const PORT = env.PORT;
 
-app.get('/', (req, res) => {
-  res.json({ok: true, message: 'Ok'});
-});
+// public directory
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname,)));
+
+// rutes
+// app.get('/', (req, res) => {
+//   res.json({ok: true, message: 'Ok'});
+// });
 
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
