a3adc45adb687c6d78fb4a1a76478c1ac35b5fb7
HappyMan17 <sebasgrisales16@gmail.com>
2024-01-16 10:31:51 -0500

    feat: cors configuration implemented.

---
+++ calendar-backend/config/constants.js
@@ -0,0 +1,11 @@
+import { envs } from "./env";
+
+
+const constants = {
+  ACCEPTED_ORIGINS: [
+    `http://localhost:${envs.PORT}`,
+    'http://localhost:5173' // front origin
+  ]
+};
+
+export default constants;
--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { envs } from './config/env.js';
 import authRouter from './routes/auth.js';
+import cors from 'cors';
 import { dbConection } from "./database/config.js";
+import constants from "./config/constants.js";
 
 const app  = express();
 const PORT = envs.PORT;
@@ -13,10 +15,21 @@ dbConection();
  * middlewares
  */
 // public directory
-app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname,)));
+app.use(express.static('public')); // app.use(express.static(path.join(__dirname,)));
 // body parser
 app.use(express.json());
+// cors
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (constants.ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true);
+    }
+    callback(new Error('Not allowed by CORS'));
+  }
+}));
 
 // Rutes
 app.use('/api/auth', authRouter);
