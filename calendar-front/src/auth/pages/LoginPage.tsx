21b1e2ae9127e2e313f7b71b65f6786a0e6e7e33
HappyMan17 <sebasgrisales16@gmail.com>
2024-01-17 13:59:43 -0500

    feat: login implementation with backend connection.

--- calendar-front/src/auth/pages/LoginPage.tsx
+++ calendar-front/src/auth/pages/LoginPage.tsx
@@ -1,17 +1,63 @@
+import { useEffect } from "react";
+import { useAuthStore, useForm } from "../../hooks";
 import "./loginPage.css";
+import Swal from "sweetalert2";
+
+const loginFormFields = {
+  loginEmail: '',
+  loginPassword: '',
+};
+
+const registerFormFields = {
+  registerName: '',
+  registerEmail: '',
+  registerPassword: '',
+  registerPassword2: '',
+};
 
 export const LoginPage = () => {
+  const { startLogin, errorMessage } = useAuthStore();
+  const { loginEmail, loginPassword, onInputChange: onLoginInputChange  }
+    = useForm(loginFormFields);
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPassword2,
+    onInputChange: onRegisterInputChange,
+  } = useForm(registerFormFields);
+
+  const loginSubmit = (event: any) => {
+    event.preventDefault();
+    startLogin({ email: loginEmail, password: loginPassword });
+  };
+
+  const registerSubmit = (event: any) => {
+    event.preventDefault();
+    console.log({ loginEmail, loginPassword });
+  };
+
+  useEffect(() => {
+    if (errorMessage) {
+      Swal.fire('Authentication Error', errorMessage, 'error');
+    }
+  }, [errorMessage]);
+
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={loginSubmit}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Correo"
+                name="loginEmail"
+                value={loginEmail}
+                onChange={onLoginInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -19,6 +65,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="loginPassword"
+                value={loginPassword}
+                onChange={onLoginInputChange}
               />
             </div>
             {/* Button */}
@@ -30,12 +79,15 @@ export const LoginPage = () => {
 
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={registerSubmit}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Nombre"
+                name="registerName"
+                value={registerName}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -43,6 +95,9 @@ export const LoginPage = () => {
                 type="email"
                 className="form-control"
                 placeholder="Correo"
+                name="registerEmail"
+                value={registerEmail}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -50,6 +105,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="registerPassword"
+                value={registerPassword}
+                onChange={onRegisterInputChange}
               />
             </div>
 
@@ -58,6 +116,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Repita la contraseña"
+                name="registerPassword2"
+                value={registerPassword2}
+                onChange={onRegisterInputChange}
               />
             </div>
 
